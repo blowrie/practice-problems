cd843fe661d14ba95c3fa749c2115dd2dc271d58
blowrie <blowrie8@gmail.com>
2018-01-23 00:35:26 -0500

    initial commit

---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Bryan Lowrie";
+            String car = "Audi";
+            String hero = "spiderman";
+
+            Console.WriteLine("Hello, my name is " + name + " and my favorite car brand is " + car + ". My favorite super hero is " + hero +  " because I like his powers.");
+        
+        }
+    }
+}
